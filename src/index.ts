624b062285ec3d95681a1f8d002cbdaf46cd2580
Tomas Giro Larraz <tllarraz@amazon.com>
2024-02-25 11:12:01 +0100

    Added prettier and made a loop to scrape all data

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,117 @@
-import { Page } from 'puppeteer';
-import puppeteer from 'puppeteer-extra';
+import { Page } from 'puppeteer'
+import puppeteer from 'puppeteer-extra'
+import * as fs from 'fs'
 
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-puppeteer.use(StealthPlugin());
-
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 
 enum Target {
-  contactInfo = "operationName=ListingContactDetailsQuery",
-  neighborhoodInfo = "async-create-search-page-state"
+  contactInfo = 'operationName=ListingContactDetailsQuery',
+  neighborhoodInfo = 'async-create-search-page-state',
 }
 
 class Scraper {
-  private page!: Page;
+  private page!: Page
 
   async start() {
-    const browser = await puppeteer.launch({ headless: false });
-    this.page = await browser.newPage();
-    await this.page.setViewport({ width: 1366, height: 768});
-    await this.page.setRequestInterception(true);
+    console.log('starting scrapper')
+    const browser = await puppeteer.launch({ headless: false })
+    this.page = await browser.newPage()
+    await this.page.setViewport({ width: 1366, height: 768 })
+    await this.page.setRequestInterception(true)
     this.page.on('request', async (request) => {
-      request.continue();
-    });
+      request.continue()
+    })
   }
 
   async query(url: string, target: string) {
     return new Promise((resolve, reject) => {
       this.page.on('response', async (response) => {
-        const url = response.url();
+        const url = response.url()
         if (url.includes(target)) {
-          console.log(response.url());
-          const json = await response.json();
-          console.log(json);
-          resolve(json);
+          const json = await response.json()
+          resolve(json)
         }
-      });
-      this.page.goto(url);
+      })
+      this.page.goto(url)
     })
   }
 }
 
+class ResultSaver {
+  zipCode: string
 
+  constructor(zipCode: string) {
+    this.zipCode = zipCode
+  }
 
-const NEIGHBORHOOD_URL = "https://www.zillow.com/jackson-ms/rentals/"
+  private get filePath(): string {
+    return `./assets/zipData/${this.zipCode}.json`
+  }
 
-;(async () => {
+  isPresent(): boolean {
+    return fs.existsSync(this.filePath)
+  }
+
+  saveData(data: any) {
+    try {
+      const outputString = JSON.stringify(data)
+      fs.writeFileSync(this.filePath, outputString)
+      console.log(`File ${this.filePath} written successfully!`)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
+
+class ZipCodeProvider {
+  private zipCodes: string[] = []
+  private filePath = './assets/zips.txt'
+  constructor() {
+    try {
+      const data = fs.readFileSync(this.filePath, 'utf-8')
+      this.zipCodes = data.split('\n').filter((zip) => zip.trim()) // Split by newlines and remove empty lines
+    } catch (err) {
+      console.error('Error reading zip code file:', err)
+      this.zipCodes = [] // Clear zip codes on error
+    }
+  }
+
+  getZipCode(): string | undefined {
+    if (this.zipCodes.length > 0) {
+      return this.zipCodes.shift() // Remove and return the first element
+    } else {
+      return undefined // Indicate no more zip codes
+    }
+  }
+}
+
+const getNeighborhoodUrl = (zipcode: string) => {
+  return `https://www.zillow.com/homes/${zipcode}`
+}
+
+function sleep(n: number) {
+  return new Promise((resolve) => setTimeout(resolve, n * 1000))
+}
+
+const NEIGHBORHOOD_URL = 'https://www.zillow.com/homes/'
+
+;(async () => {
   const scrappy = new Scraper()
   await scrappy.start()
 
-  const json = await scrappy.query(NEIGHBORHOOD_URL, Target.neighborhoodInfo)
-  //console.log(JSON.stringify(json))
-})();
-
+  const zipProvider = new ZipCodeProvider()
+  let zip = zipProvider.getZipCode()
+  while (zip) {
+    const resultSaver = new ResultSaver(zip)
+    if (!resultSaver.isPresent()) {
+      const json = await scrappy.query(
+        getNeighborhoodUrl(zip),
+        Target.neighborhoodInfo
+      )
+      resultSaver.saveData(json)
+      sleep(0.5)
+    }
+    zip = zipProvider.getZipCode()
+  }
+})()
